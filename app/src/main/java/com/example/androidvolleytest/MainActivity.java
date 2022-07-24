08ec3f42b48e11d43a72c40e7a323a64b1dbbb93
Md Hasib Askari <mdhsbaskari@gmail.com>
2022-07-25 01:18:58 +0600

    Initial commit

---
+++ app/src/main/java/com/example/androidvolleytest/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.androidvolleytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    String url = "https://api.chucknorris.io/jokes/random";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+
+// Request a string response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            textView.setText(response.getString("value"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText(error.toString());
+                    }
+                }
+        );
+
+// Add the request to the RequestQueue.
+        requestQueue.add(jsonObjectRequest);
+    }
+}
